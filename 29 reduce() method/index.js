f664f5900477896cd34b4a18b6d7a05bfdcce275
Aman-raj23 <23raj.aman03@gmail.com>
2025-08-21 22:54:01 +0530

    updated

--- 29 reduce() method/index.js
+++ 29 reduce() method/index.js
@@ -14,10 +14,19 @@ console.log(`$${total}`);
 
 let fruits = ["apple", "banana", "apple", "orange", "banana", "apple"];
 
-const occurance = (acc, fruit) => {
+const occurance = (acc, fruit) => {                     // function declaration 
      acc[fruit] = (acc[fruit] || 0) + 1;
     return acc;
 }
 
 let count = fruits.reduce(occurance, {});
 console.log(count);
+
+
+
+
+let count1 = fruits.reduce((acc, fruit) => {             // function expression
+     acc[fruit] = (acc[fruit] || 0) + 1;
+    return acc;
+}, {});
+console.log(count1);
