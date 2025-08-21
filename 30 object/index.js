303fae17748489938b5751fd2f5eb8a8ff540e44
Aman-raj23 <23raj.aman03@gmail.com>
2025-08-22 00:58:19 +0530

    updated

--- 30 object/index.js
+++ 30 object/index.js
@@ -22,3 +22,27 @@ console.log(`Student: ${person1.isStudent}`);
 console.log(person2.firstName);
 console.log(person2.lastName);
 console.log(`Student: ${person2.isStudent}`);
+
+
+
+//========== function inside a object =========
+
+const person3 = {
+    Name: "Snow",
+    age: 22,
+    work: function(name) { console.log(`${name} is Working!`)},
+    fun: function() { console.log(`and he is not having fun`)}
+
+}
+
+const person4 = {
+    Name: "Sam",
+    age: 24,
+    work: (name) => { console.log(`${name} is not Working!`)},
+    fun: () => { console.log(`He is having fun`)}
+}
+
+person3.work("Snow");
+person3.fun();
+person4.work("Sam");
+person4.fun();
---
+++ 31 this keyword/index.js
@@ -0,0 +1,20 @@
+const person1 = {
+    name: "Sam",
+    age: 24,
+    sayHello: function() { console.log(`hey! I'm ${this.name}`)},       // this.name = person1.name
+    work: function() { console.log(`${this.name} is not Working!`)}
+}
+person1.sayHello();
+person1.work();
+
+
+
+//======== Doesn't work with arrow functions ========
+
+
+const person2 ={
+    age: 20,
+    sayAge: () => { console.log(`I'm ${this.age} years old`)}          // undefined value with arrow functions
+}
+
+person2.sayAge();
