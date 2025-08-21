4fc8057278760e697549345106711d37c0008f71
Aman-raj23 <23raj.aman03@gmail.com>
2025-08-21 23:44:16 +0530

    initial commit

---
+++ 30 object/index.js
@@ -0,0 +1,24 @@
+const person1 = {
+    firstName: "Harry",
+    lastName: "Potter", 
+    age: 17,
+    isStudent: true,
+}
+
+
+const person2 = {
+    firstName: "John",
+    lastName: "Wick", 
+    age: 30,
+    isStudent: false,
+}
+
+
+console.log(person1.firstName);
+console.log(person1.lastName);
+console.log(person1.age);
+console.log(`Student: ${person1.isStudent}`);
+
+console.log(person2.firstName);
+console.log(person2.lastName);
+console.log(`Student: ${person2.isStudent}`);
