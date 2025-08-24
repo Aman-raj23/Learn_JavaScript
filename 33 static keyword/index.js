fd59c7dc26c1f3247c24d637410b4ef571323cd8
Aman-raj23 <23raj.aman03@gmail.com>
2025-08-24 21:57:29 +0530

    updated

--- 33 static keyword/index.js
+++ 33 static keyword/index.js
@@ -4,10 +4,19 @@ class Person{
     static sayName(name){
         console.log(`My name is ${name}.`);
     }
+    
+    static sayAge() {
+        console.log(`and i am ${Person.age} years old`);
+    }
 }
 
+// let name = new Person();
+// console.log(name.sayName(Aman));        // Error if you try with object
+
 console.log(Person.age);
-Person.sayName("Raj");
+Person.sayName("Raj");      // Call static methods directly on class
+Person.sayAge();
+
 
 
 //=========================
