5a451903ea4e2ad30e4811fdc28c577b6922d705
Aman-raj23 <23raj.aman03@gmail.com>
2025-09-03 19:11:14 +0530

    initial commit

---
+++ 43 ES6 Modules/main.js
@@ -0,0 +1,4 @@
+import { add, subtract } from './math.js';  // Importing specific functions
+
+console.log(add(5, 3));       
+console.log(subtract(10, 4)); 
---
+++ 43 ES6 Modules/math.js
@@ -0,0 +1,9 @@
+// Exporting functions
+
+export function add(a, b) {
+    return a + b;
+}
+
+export function subtract(a, b) {
+    return a - b;
+}
