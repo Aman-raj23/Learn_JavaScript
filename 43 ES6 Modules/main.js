f8c103cdc546d181dc0b28ddc87d0a0ec0b1d61b
Aman-raj23 <23raj.aman03@gmail.com>
2026-07-21 15:43:00 +0530

    update inline css

--- 43 ES6 Modules/main.js
+++ 43 ES6 Modules/main.js
@@ -1,4 +1,4 @@
 import { add, subtract } from './math.js';  // Importing specific functions
 
-console.log(add(5, 3));       
-console.log(subtract(10, 4)); 
+console.log(add(5, 3));
+console.log(subtract(10, 4));
