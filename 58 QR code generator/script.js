985612319f9fb6498648b107e183af9532e41a7d
Aman-raj23 <23raj.aman03@gmail.com>
2026-07-25 17:22:01 +0530

    initial commit

---
+++ 58 QR code generator/script.js
@@ -0,0 +1,21 @@
+const qrText = document.getElementById("qrText");
+const generateBtn = document.getElementById("generateBtn");
+const qrImage = document.getElementById("qrImage");
+
+generateBtn.addEventListener("click", generateQR);
+
+function generateQR(){
+
+    let text = qrText.value.trim();
+
+    if(text === ""){
+        alert("Please enter some text");
+        return;
+    }
+
+    qrImage.src =
+    `https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=${encodeURIComponent(text)}`;
+
+    qrImage.style.display = "block";
+
+}
