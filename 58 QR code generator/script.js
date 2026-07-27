fba8b87e8d9e48590ba815dbedf0744531c7424c
Aman-raj23 <23raj.aman03@gmail.com>
2026-07-27 12:38:43 +0530

    improve QR generation function and add alert for empty input style: adjust button margin for better spacing

--- 58 QR code generator/script.js
+++ 58 QR code generator/script.js
@@ -8,20 +8,33 @@ const downloadBtn = document.getElementById("downloadBtn");
 
 generateBtn.addEventListener("click", generateQR);
 
-function generateQR(){
+function generateQR() {
 
     let text = qrText.value.trim();
 
-    if(text === ""){
+    if (text === "") {
         alert("Please enter some text");
         return;
     }
 
     qrImage.src =
-    `https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=${encodeURIComponent(text)}`;
+        `https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=${encodeURIComponent(text)}`;
 
     qrImage.style.display = "block";
     downloadBtn.style.display = "block";
 
 
 }
+
+downloadBtn.addEventListener("click", function () {
+
+    const link = document.createElement("a");
+
+    link.href = qrImage.src;
+    link.download = "QRCode.png";
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+});
